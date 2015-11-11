a8ee6fde12667ddb15f14f8228dc9c7bcd9a32b2
江清清 <jiangqqlmj@users.noreply.github.com>
2015-11-11 13:44:38 +0800

    +):Volley框架新增字符串，文本请求封装类 Fdv_StringRequest

---
+++ Volley/src/main/java/com/chinaztt/fdv/Fdv_StringRequest.java
@@ -0,0 +1,13 @@
+package com.chinaztt.fdv;
+
+/**
+ * 当前类注释:Volley 字符串、文本数据请求封装类
+ * 项目名：FastDev4Android
+ * 包名：com.chinaztt.fdv
+ * 作者：江清清 on 15/11/11 13:43
+ * 邮箱：jiangqqlmj@163.com
+ * QQ： 781931404
+ * 公司：江苏中天科技软件技术有限公司
+ */
+public class Fdv_StringRequest {
+}
