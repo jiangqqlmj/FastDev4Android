eee984169b51a791ed34f7a1b75be0fec3757b40
江清清 <jiangqqlmj@users.noreply.github.com>
2015-11-28 09:15:20 +0800

    +):1.更新SwipeRefreshLayout+RecyclerView+CardView使用实例代码功能;      2.新增演示gif图

--- app/src/main/java/com/chinaztt/fda/test/ComInstance/ComInstanceAdapter.java
+++ app/src/main/java/com/chinaztt/fda/test/ComInstance/ComInstanceAdapter.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.chinaztt.fda.ui.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,29 +21,66 @@ import java.util.List;
  * QQ： 781931404
  * 公司：江苏中天科技软件技术有限公司
  */
-public class ComInstanceAdapter extends  RecyclerView.Adapter<ComInstanceAdapter.ItemViewHolder> {
+public class ComInstanceAdapter extends  RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context mContext;
     private List<InstanceBean> mInstanceBeans;
     private LayoutInflater mInflater;
+
+    //布局新增一项类别
+    //普通ITEM
+    private static final int ITEM_VIEW=1;
+    //FOOT ITEM
+    private static final int FOOT_VIEW=2;
+
     public ComInstanceAdapter(Context context,List<InstanceBean> pInstanceBeans){
         this.mContext=context;
         this.mInstanceBeans=pInstanceBeans;
         mInflater=LayoutInflater.from(this.mContext);
     }
     @Override
-    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view=mInflater.inflate(R.layout.com_instance_item_layout,parent,false);
-        return new ItemViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        if (viewType == ITEM_VIEW) {
+            final View view = mInflater.inflate(R.layout.com_instance_item_layout, parent, false);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onItemClickListener != null) {
+                        onItemClickListener.onItemClick(view, (int) view.getTag());
+                    }
+                }
+            });
+            return new ItemViewHolder(view);
+        } else if (viewType == FOOT_VIEW) {
+            View view = mInflater.inflate(R.layout.instance_load_more_layout, parent, false);
+            return new FootViewHolder(view);
+        }
+        return null;
     }
     @Override
-    public void onBindViewHolder(ItemViewHolder holder, int position) {
-        holder.item_img.setImageResource(mInstanceBeans.get(position).getImg());
-        holder.item_tv.setText(mInstanceBeans.get(position).getTitle());
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof ItemViewHolder){
+            ((ItemViewHolder)holder).item_img.setImageResource(mInstanceBeans.get(position).getImg());
+            ((ItemViewHolder)holder).item_tv.setText(mInstanceBeans.get(position).getTitle());
+            holder.itemView.setTag(position);
+        }else if(holder instanceof FootViewHolder){
+            //上拉加载更多布局数据绑定
+        }
+
 
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position + 1 == getItemCount()) {
+            return FOOT_VIEW;
+        } else {
+            return ITEM_VIEW;
+        }
+    }
+
     @Override
     public int getItemCount() {
-        return mInstanceBeans!=null?mInstanceBeans.size():0;
+        return mInstanceBeans!=null?mInstanceBeans.size()+1:0;
     }
 
     public static class ItemViewHolder extends RecyclerView.ViewHolder{
@@ -54,7 +91,52 @@ public class ComInstanceAdapter extends  RecyclerView.Adapter<ComInstanceAdapter
             item_img=(ImageView)itemView.findViewById(R.id.item_img);
             item_tv=(TextView)itemView.findViewById(R.id.item_tv);
         }
-
+    }
+    /**
+     * 底部FootView布局
+     */
+    public static class FootViewHolder extends  RecyclerView.ViewHolder{
+        private TextView foot_view_item_tv;
+        public FootViewHolder(View view) {
+            super(view);
+            foot_view_item_tv=(TextView)view.findViewById(R.id.foot_view_item_tv);
+        }
     }
 
+    /**
+     * Item 点击监听回调接口
+     */
+    public interface OnItemClickListener {
+        void onItemClick(View view,int position);
+    }
+    private OnItemClickListener onItemClickListener;
+
+    public OnItemClickListener getOnItemClickListener() {
+        return onItemClickListener;
+    }
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    /**
+     * 进行下拉刷新数据添加 并且刷新UI
+     * @param pInstanceBeans
+     */
+    public void addRefreshBeans(List<InstanceBean> pInstanceBeans){
+         List<InstanceBean> temp=new ArrayList<InstanceBean>();
+         temp.addAll(pInstanceBeans);
+         temp.addAll(mInstanceBeans);
+         mInstanceBeans.removeAll(mInstanceBeans);
+         mInstanceBeans.addAll(temp);
+         notifyDataSetChanged();
+    }
+
+    /**
+     * 进行上拉加载更多 并且刷新UI
+     * @param pInstanceBeans
+     */
+    public void addMoreBeans(List<InstanceBean> pInstanceBeans){
+        mInstanceBeans.addAll(pInstanceBeans);
+        notifyDataSetChanged();
+    }
 }
