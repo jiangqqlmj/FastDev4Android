16db42db5adb5e23af0cb87bbcd426dd3b04f6c0
江清清 <jiangqqlmj@users.noreply.github.com>
2015-12-04 15:59:26 +0800

    +):1.新增fdv_okhttp网络请求封装工具类库      2.新增okhttp功能测试入口相关类

---
+++ app/src/main/java/com/chinaztt/fda/test/okhttp/OkhttpDemoActivity.java
@@ -0,0 +1,21 @@
+package com.chinaztt.fda.test.okhttp;
+
+import android.os.Bundle;
+
+import com.chinaztt.fda.ui.base.BaseActivity;
+
+/**
+ * 当前类注释:Okhttp 网络请求框架 实例demo功能测试入口
+ * 项目名：FastDev4Android
+ * 包名：com.chinaztt.fda.test.okhttp
+ * 作者：江清清 on 15/12/4 15:56
+ * 邮箱：jiangqqlmj@163.com
+ * QQ： 781931404
+ * 公司：江苏中天科技软件技术有限公司
+ */
+public class OkhttpDemoActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
